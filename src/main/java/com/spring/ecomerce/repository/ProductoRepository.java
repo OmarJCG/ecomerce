76b5b64e69222a9a44de43bb8c8f776fff5859d4
Omar Carrasco <omarcarrascoguerrero@gmail.com>
2024-09-16 17:33:06 -0500

    crear repository, service, method show

---
+++ src/main/java/com/spring/ecomerce/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.spring.ecomerce.repository;
+
+import com.spring.ecomerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+}
